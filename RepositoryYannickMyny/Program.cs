790afca0f17b62735d54c76e8e1c82836e8cd6a0
MynyY <mynyyannick2@hotmail.com>
2020-02-14 14:45:10 +0100

    Add project files.

---
+++ RepositoryYannickMyny/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RepositoryYannickMyny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
